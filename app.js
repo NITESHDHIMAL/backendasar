754528f75e3603396e4ef344899357367d7a2186
NITESHDHIMAL <niteshdhimal18@gmail.com>
2025-10-09 17:02:56 +0545

    multer setup and use for file setup

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ require('dotenv').config()
 const express = require('express');
 const connectDatabase = require('./database');
 const Product = require('./model/productModel');
+const multer = require('multer');
+const { storage } = require('./middleware/multerConfig');
 
 const app = express();
 
 app.use(express.json());
 
 connectDatabase();
+
+// for file uploading 
+const upload = multer({ storage: storage  })
 
 
 // crud => create read update delete 
@@ -19,13 +24,19 @@ app.get( '/product', async (req,res)=> {
     })
 })
 
-app.post('/product', (req, res)=> {
+app.post('/product', upload.single('image'), async (req, res)=> {
     console.log(req.body)
+    console.log(req.file)
     const { name, price, description, image } = req.body
-    Product.create({name, price, description, image})
+
+    const filename = req.file.filename
+    console.log("this is filename:", filename)
+
+    const product = await Product.create({name, price, description, image: filename})
+
     res.status(201).json({
         message: "Product created successfully!",
-        data: req.body
+        data: product
     })
 })
 
